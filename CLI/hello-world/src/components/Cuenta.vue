61c76839a38cf351ba8c8fd4180900b85a7ec31d
crisLeo-P01 <cristianleopaiva@gmail.com>
2022-06-30 20:05:40 -0300

    Trabajando con vue/cli

---
+++ CLI/hello-world/src/components/Cuenta.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Tpo de cuenta: {{ cuenta }}</h2>
+  <h2>Saldo: {{ saldo }}</h2>
+  <h3>Estado de cuenta: {{ estado ? 'Activa' : 'Desactivada' }}</h3>
+  <div v-for="(servicio, index) in servicios" :key="index">{{ index + 1 }} - {{ servicio }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cuenta: 'Visa',
+      saldo: 1000,
+      estado: true,
+      servicios: ['giros', 'pagos', 'transferencias'],
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ CLI/hello-world/src/components/Titulo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ texto }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Titulo',
+  props: {
+    texto: String,
+  }
+}
+</script>
+
+<!-- scoped significa que solo va a afectar al h1 de este componente -->
+<style scoped> 
+  h1 {
+    color: crimson;
+  }
+</style>
